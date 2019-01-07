bd96186985fa46c39b1ad8666a4bc2aaddbac41a
Logan Wang <loganxwang@hotmail.com>
2019-01-07 08:41:51 -0800

    updated react and react-dom

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {Field, reduxForm, focus} from 'redux-form';
-import {registerUser} from '../actions/users';
-import {login} from '../actions/auth';
+import { Field, reduxForm, focus } from 'redux-form';
+import { registerUser } from '../actions/users';
+import { login } from '../actions/auth';
 import Input from './input';
-import {required, nonEmpty, matches, length, isTrimmed} from '../validators';
-const passwordLength = length({min: 10, max: 72});
+import { required, nonEmpty, matches, length, isTrimmed } from '../validators';
+const passwordLength = length({ min: 6, max: 72 });
 const matchesPassword = matches('password');
 
 export class RegistrationForm extends React.Component {
     onSubmit(values) {
-        const {username, password, firstName, lastName} = values;
-        const user = {username, password, firstName, lastName};
+        const { username, password, firstName, lastName } = values;
+        const user = { username, password, firstName, lastName };
         return this.props
             .dispatch(registerUser(user))
             .then(() => this.props.dispatch(login(username, password)));
